cecb93e6175342db5ef5812c6a322c40bbb4530a
thejobsmarket <152418647+thejobsmarket@users.noreply.github.com>
2025-11-10 22:14:24 +0000

    fixed issue

--- examples/component-describe/src/lib.rs
+++ examples/component-describe/src/lib.rs
@@ -1,12 +1,18 @@
+#![cfg_attr(not(target_arch = "wasm32"), allow(dead_code))]
+
+#[cfg(target_arch = "wasm32")]
 use exports::greentic::component::describe_v1;
 
+#[cfg(target_arch = "wasm32")]
 wit_bindgen::generate!({
     path: "../../crates/greentic-interfaces/wit/greentic/component@1.0.0",
     world: "component",
 });
 
+#[cfg(target_arch = "wasm32")]
 struct DescribeComponent;
 
+#[cfg(target_arch = "wasm32")]
 impl describe_v1::Guest for DescribeComponent {
     fn describe_json() -> std::string::String {
         r#"{"name":"component-describe","versions":[{"version":"1.0.0","schema":"{}"}]}"#
@@ -14,4 +20,5 @@ impl describe_v1::Guest for DescribeComponent {
     }
 }
 
+#[cfg(target_arch = "wasm32")]
 export!(DescribeComponent);
